057e9b853bf43feef766db096e07814020ce7c93
Richard Howell-Peak <richardhp@bitbucket.org>
2018-08-03 17:15:27 +0100

    Added some helper code

--- game_engine.js
+++ game_engine.js
@@ -66,7 +66,7 @@ function GameEngine() {
 		//Update and draw the active scene if we have it
 		if ( this.scnActive != null ) {
 			this.scnActive.fnUpdate();
-			this.scnActive.fnRender();
+			this.scnActive.fnDraw();
 		}
 		//The timeout is based on the FPS
 		setTimeout( this.fnLoop, 1000 / this.intFPS );
@@ -79,11 +79,20 @@ function GameEngine() {
 function Scene(name) {
 	//The name of the scene
 	this.strName = name;
+	this.lstSprites = [];
+	this.domObject = document.createElement("div");
 	/**
 	 * The load function, load images etc.
 	 */
 	this.fnLoad = function() {
 		
+	}
+	/**
+	 * Add a sprite to the scene
+	 */
+	this.fnAddSprite = function (s ) {
+		this.lstSprites.push(s);
+		this.domObject.appendChild(s.img);
 	}
 	/**
 	 * Reset the scene, like restart level etc.
@@ -113,13 +122,15 @@ function Scene(name) {
 	 * Call this when a scene is switched away from
 	 */
 	this.fnDeactivate = function() {
-		
+		//Unload all objects off the DOM
+		document.body.removeChild(this.domObject);
 	}
 	/**
 	 * Call this when a scene is switched to
 	 */
 	this.fnActivate = function() {
-		
+		//Load all objects to DOM
+		document.body.appendChild(this.domObject);
 	}
 }
 
@@ -129,23 +140,42 @@ function Scene(name) {
  * The game object class
  */
 function Sprite(name) {
+	//These are object attributes
 	this.strName = name;
 	this.x = 0;
 	this.y = 0;
 	this.width = 10;
 	this.height = 10;
 	this.rotation = 0;
+	//The DOM, or "Document Object Model" are the things on the actual screen.
+	//In other words, the actual html document.  "body" is the main one, also "head" and "title" etc.
+	//Anything displayed on the screen needs to be a child element of "body"
+	this.img = null;
 	
+	/**
+	 * Load image function
+	 */
+	this.fnLoadImage = function(strFile) {
+		this.img = document.createElement('img');
+		this.img.src = strFile;
+	}
 	/**
 	 * Update function
 	 */
 	this.fnUpdate = function() {
-		
+		this.x++;
 	}
 	/**
 	 * Draw this to screen
 	 */
 	this.fnDraw = function() {
+		//Here we must update the this.img.style properties so it's in the correct location
+		//We want (x,y) to represent the centre of the object.  HTML works on top left of image
+		//however.  Also must convert to integer then to string so can add "px" to it to tell HTML it's in pixels.
+		this.img.style.left = parseInt(this.x - this.width / 2).toString() + "px";
+		this.img.style.top = "px";
+		this.img.style.width = "px";
+		this.img.style.height = "px";
 		
 	}
 }
